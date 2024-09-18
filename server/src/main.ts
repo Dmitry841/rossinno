ee9aaba94307ff072f8231ba3e41a53dbfeb60ed
Дмитрий Абдалин <abdalin_d@liman-tech.ru>
2024-09-18 11:26:16 +0300

    fix

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,12 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
 async function bootstrap() {
-  const port = process.env.PORT || 3000;
+  const portArgIndex = process.argv.indexOf('port');
+  console.log(8, process.argv);
+  const port =
+    portArgIndex >= 0
+      ? process.argv[portArgIndex + 1]
+      : process.env.PORT || 3000;
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setViewEngine('html');
